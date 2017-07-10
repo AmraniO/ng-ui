1b02029e33c3d254220d1836dd298c63d0253279
AmraniO <o.amrani@gmail.com>
2017-07-10 22:46:25 +0200

    feat(data-table): add prototype

---
+++ src/app/modules/data-table/data-table.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "nui-data-table",
+    templateUrl: "data-table.component.html"
+})
+export class DataTableComponent {
+    
+}
---
+++ src/app/modules/data-table/data-table.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { DataTableComponent } from "./data-table.component";
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        DataTableComponent
+    ],
+    exports: [
+        DataTableComponent
+    ]
+})
+export class DataTableModule { }
