b7facf4e73598aa703fedb2e63b62b788914dd0f
AmraniO <o.amrani@gmail.com>
2017-07-12 22:14:43 +0200

    chore(nav-menu): show example of nav-menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  navsOp = [
+        {
+            name: "dashboard",
+            label: "Dashboard",
+            component: null
+        },     
+        {
+            name: "customer",
+            label: "Customer",
+            component: "operation/customer"
+        },
+        {
+            name: "supplier",
+            label: "Supplier",
+            component: null
+        },
+        {
+            name: "invoice",
+            label: "Invoice",
+            component: "operation/invoice"
+        },
+        {
+            name: "order",
+            label: "Order",
+            component: "operation/order"
+        }
+  ]
+
+  navsAdmin = [
+      {
+          name: "vat",
+          label: "VAT",
+          component: null
+      }        
+  ]
+
+  navsConfig = [
+      {
+          name: "user",
+          label: "User",
+          component: "configuration/user"
+      }        
+  ]
+
+  navCategories = [
+        { name: "operation", label: "Operation", icon: "apps", navs: this.navsOp, open: true },
+        { name: "administration", label: "Administration", icon: "view_list", navs: this.navsAdmin, open: false },
+        { name: "configuration", label: "Configuration", icon: "settings", navs: this.navsConfig, open: false }
+    ]
 }
